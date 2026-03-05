1098c9289156ed414eff0a1b7e455c2ab948d0ac
Olga Zenkina <zeninindia@gmail.com>
2026-03-06 00:13:44 +0300

    first commit of fourth homework

---
+++ lesson_05/lesson05_task2.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+browser = webdriver.Chrome()
+browser.get("http://uitestingplayground.com/dynamicid")
+
+sleep(2)
+browser.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+sleep(2)
+browser.quit()
---
+++ lesson_05/lesson05_task3.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+
+browser = webdriver.Firefox()
+browser.get("http://the-internet.herokuapp.com/inputs")
+
+sleep(2)
+input_field = browser.find_element(By.CSS_SELECTOR, "[type='number']")
+input_field.clear()
+input_field.send_keys("123")
+sleep(2)
+input_field.clear()
+input_field.send_keys("345")
+sleep(2)
+browser.quit()

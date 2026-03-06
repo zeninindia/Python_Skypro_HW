648789ea446cd28bb0d32a079d138b4f8c961260
Olga Zenkina <zeninindia@gmail.com>
2026-03-06 13:44:41 +0300

    second commit of 06 homework

--- 06_lesson/06_lesson_task01.py
+++ 06_lesson/06_lesson_task01.py
@@ -1,13 +1,19 @@
-import until
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 browser = webdriver.Chrome()
-browser.implicitly_wait(30)
 browser.get("http://uitestingplayground.com/ajax")
 
+pause = WebDriverWait(browser, 40)
+
 browser.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+
+pause.until(
+    EC.text_to_be_present_in_element( (By.CSS_SELECTOR, '[class="bg-success"]'), "Data loaded with AJAX get request.")
+)
 
 txt = browser.find_element(By.CSS_SELECTOR, '[class="bg-success"]')
 print(txt.text)
--- 06_lesson/06_lesson_task02.py
+++ 06_lesson/06_lesson_task02.py
@@ -9,12 +9,16 @@ from selenium.webdriver.support import expected_conditions as EC
 browser = webdriver.Chrome()
 
 browser.get(" http://uitestingplayground.com/textinput")
-
+pause = WebDriverWait(browser, 40)
 input_field = browser.find_element(By.CSS_SELECTOR, '[class="form-control"]')
 
 input_field.send_keys("SkyPro")
-browser.implicitly_wait(50)
+
 browser.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+
+pause.until(
+    EC.text_to_be_present_in_element( (By.CSS_SELECTOR,"#updatingButton"), "SkyPro")
+)
 
 txt = browser.find_element(By.CSS_SELECTOR, '#updatingButton')
 

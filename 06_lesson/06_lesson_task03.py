b6c7f0ddef381303911acb0f0669b450fda21985
Olga Zenkina <zeninindia@gmail.com>
2026-03-06 15:53:31 +0300

    therd commit of 06 homework

--- 06_lesson/06_lesson_task03.py
+++ 06_lesson/06_lesson_task03.py
@@ -7,8 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 browser = webdriver.Chrome()
 browser.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-element = WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#award"))
+element = WebDriverWait(browser, 40).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#landscape"))
     )
 src = browser.find_element(By.CSS_SELECTOR, '#award')
 print(src.get_attribute('src'))

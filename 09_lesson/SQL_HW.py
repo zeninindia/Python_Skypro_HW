29406340a4225dff0872a3294eb79d567f29756a
Olga Zenkina <zeninindia@gmail.com>
2026-03-21 02:09:22 +0300

    first commit of _09

---
+++ 09_lesson/SQL_HW.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.sql import text
+from sqlalchemy.orm import sessionmaker
+
+db_connection_string = "postgresql://postgres:Gravel@localhost:5432/QA"
+db = create_engine(db_connection_string)
+
+def test_insert():
+    sviaz = db.connect()
+    transaction = sviaz.begin()
+    sql_nadpis = text("insert into subject(\"subject_title\") values (:new_title)")
+    new_data = {"new_title": "Scottish"}
+    otvet = sviaz.execute(sql_nadpis, new_data)
+    transaction.commit()
+
+    sviaz.close()
+
+def test_update():
+    sviaz = db.connect()
+    transaction = sviaz.begin()
+    sql_nadpis = text("UPDATE subject SET subject_id = :new_id WHERE subject_title = :new_title")
+    new_data = {"new_id": "17", "new_title": "Scottish"}
+    otvet = sviaz.execute(sql_nadpis, new_data)
+    transaction.commit()
+
+    sviaz.close()
+
+def test_delete():
+    sviaz = db.connect()
+    transaction = sviaz.begin()
+
+    sql = text("DELETE FROM subject WHERE subject_id = :id")
+    sviaz.execute(sql, {"id": 17})
+
+    transaction.commit()
+    sviaz.close()
